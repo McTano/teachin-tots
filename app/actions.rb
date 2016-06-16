00452fcf7c95254b2036890738940256fe3ed08b
triple-eh <aleckaleksandrov@gmail.com>
2016-06-16 12:42:00 -0700

    temp changes to game

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,21 @@
 # Homepage (Root path)
+helpers do
+  def current_game
+    Game.find(session[:id]) if session[:id]
+  end
+
+  def start_game
+    game = Game.new
+    session[:id] = game.id
+    game
+  end
+end
+
 get '/' do
   erb :index
+end
+
+post '/play' do
+  @game = start_game unless current_game
+  erb :"play/index"
 end
--- app/lib/game.rb
+++ app/lib/game.rb
@@ -1,3 +1,33 @@
 class Game
 
+  @@games = []
+
+# TEST
+  attr_reader :id
+  
+  def initialize
+    @@games << self
+    @id = @@games.length - 1
+  end
+
+  def current_question
+    self
+  end
+  
+  def choices
+      ["https://img.buzzfeed.com/buzzfeed-static/static/2014-04/enhanced/webdr03/4/16/enhanced-26552-1396642701-1.jpg?no-auto",
+        "https://img.buzzfeed.com/buzzfeed-static/static/2014-04/enhanced/webdr06/4/16/enhanced-11136-1396643149-13.jpg?no-auto",
+        "https://img.buzzfeed.com/buzzfeed-static/static/2014-04/enhanced/webdr06/9/14/enhanced-631-1397069470-19.jpg?no-auto"
+      ]
+  end
+
+  def self.find(id)
+    @@games[id]
+  end
+
+
+# END OF TEST CODE
+
 end
+
+puts Game.new.current_question.choices
