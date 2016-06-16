6e6f2887f94f47d7db7366ef3b9b351a03103b60
triple-eh <aleckaleksandrov@gmail.com>
2016-06-16 16:09:25 -0700

    checks right/wrong answer

--- app/actions.rb
+++ app/actions.rb
@@ -1,17 +1,26 @@
-# Homepage (Root path)
 helpers do
   def start_game
-    game = Game.new
-    session[:id] = game.id
-    game
+    session[:game] = Game.new
   end
 end
 
 get '/' do
+  session.clear
   erb :index
 end
 
 post '/play' do
+  @game = session[:game]  
+  if params[:choice_index] && @game.current_question.correct?(params[:choice_index].to_i)
+    @border_color = 'green'
+  else
+    @border_color = 'red'
+  end
+  @previous_answer = params[:choice_index].to_i
+  erb :"play/index"
+end
+
+get '/play' do
   @game = start_game
   erb :"play/index"
 end
