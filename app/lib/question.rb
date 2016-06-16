de31b55a60c30c187c3b201aa8cf9da514ff8406
triple-eh <aleckaleksandrov@gmail.com>
2016-06-16 13:28:44 -0700

    fixed jpg issues

--- app/lib/question.rb
+++ app/lib/question.rb
@@ -4,9 +4,9 @@ class Question
 
   def initialize
     @word = "cat"
-    @correct_img = "http://imgur.com/H10X3rQ"
-    @dummy_1 = "http://i.imgur.com/A2CL0Ah.jpg"
-    @dummy_2 = "http://imgur.com/gallery/rCoolmO"
+    @correct_img = "http://i.imgur.com/H10X3rQ.jpg"
+    @dummy_1 = "http://i.imgur.com/PuLsUZI.jpg"
+    @dummy_2 = "http://i.imgur.com/rCoolmO.jpg"
     @choices = [@correct_img, @dummy_1, @dummy_2].shuffle
   end
 
