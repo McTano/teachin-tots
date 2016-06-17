648a93ad043966de55a106cdb0a9303b0713b202
triple-eh <aleckaleksandrov@gmail.com>
2016-06-16 18:24:45 -0700

    game end works

--- app/actions.rb
+++ app/actions.rb
@@ -11,9 +11,6 @@ end
 
 post '/play' do
   #set game to current game via session
-  @game = session[:game] 
-
-  redirect '/end' unless @game.current_question
 
   unless params[:button] == 'next'
     #if choice of an answer is passed in params and the choice is correct set border to green otherwise to red
@@ -23,9 +20,11 @@ post '/play' do
       @border_color = 'red'
     end
     #record the choice of the previous answer to an instance variable to pass on to play/index.erb
+    @game.increment_tries
     @previous_answer = params[:choice_index].to_i
   else
     @game.next_question
+    redirect '/end' unless @game.current_question
   end
 
   erb :'play/index'
@@ -37,5 +36,6 @@ get '/play' do
 end
 
 get '/end' do
+  @game = session[:game]
   erb :'end/index'
 end
