1538952bd9d49a482c696626faa79324efbd9ee2
tristan <tsmackinlay@gmail.com>
2016-06-17 14:43:25 -0700

    added writers to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,21 @@ Concept.create!(word: "monkey", image: "http://i.imgur.com/JRcTgKz.jpg", categor
 Concept.create!(word: "seal", image: "http://i.imgur.com/NPOdo1K.jpg", category: "animals")
 Concept.create!(word: "sea lion", image: "http://i.imgur.com/2Es33lH.jpg", category: "animals")
 Concept.create!(word: "racoon", image: "http://i.imgur.com/uEwP6Hr.jpg", category: "animals")
+
+# Notable Writers
+
+Concept.create(word: "Mordecai Richler", image: "http://i.imgur.com/gOca5wc.jpg?1", category: "writers")
+Concept.create(word: "Lord Byron", image: "http://i.imgur.com/cPWf9EI.jpg?1", category: "writers")
+Concept.create(word: "Langston Hughes", image: "http://i.imgur.com/cXsBC0E.jpg?1", category: "writers")
+Concept.create(word: "Jane Austen", image: "http://i.imgur.com/eUBhNzz.jpg?1", category: "writers")
+Concept.create(word: "Ernest Hemingway", image: "http://i.imgur.com/szyYyvI.jpg?1", category: "writers")
+Concept.create(word: "Emily Bronte", image: "http://i.imgur.com/O1oamH5.jpg?1", category: "writers")
+Concept.create(word: "Charlotte Bronte", image: "http://i.imgur.com/G95wcxf.jpg?1", category: "writers")
+Concept.create(word: "Margaret Atwood", image: "http://i.imgur.com/g2jgAVD.jpg?1", category: "writers")
+Concept.create(word: "Isaac Asimov", image: "http://i.imgur.com/yLqJR4i.jpg?1", category: "writers")
+Concept.create(word: "Ursula K. Le Guin", image: "http://i.imgur.com/bWI1n4O.jpg?1", category: "writers")
+Concept.create(word: "Gertrude Stein", image: "http://i.imgur.com/X6FPbtx.jpg?1", category: "writers")
+Concept.create(word: "William Shakespeare", image: "http://i.imgur.com/WpNPWeP.jpg", category: "writers")
+Concept.create(word: "Edgar Allan Poe", image: "http://i.imgur.com/BPPYlO6.jpg?1", category: "writers")
+Concept.create(word: "Murakami", image: "http://i.imgur.com/aqRnaop.jpg?1", category: "writers")
+Concept.create(word: "Mark Twain", image: "http://i.imgur.com/L7TuRIe.jpg", category: "writers")
