0bcaa9bb7a138a902cf9e8db97a1a05236280c66
triple-eh <aleckaleksandrov@gmail.com>
2016-06-18 12:46:32 -0700

    moved script into application.js

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,46 @@
-$(document).ready(function() {
+$(function () {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('body')
+    .on('change', 'input[type=radio]', function () {
+      var radioButton = $(this);
+      radioButton.closest('label').addClass('selected').siblings('label').removeClass('selected');
+      radioButton.closest('form').trigger('submit');
+    })
+    .on('submit', 'form', function (event) {
+      event.preventDefault();
+      var form = $(this);
+      // form.find('button').prop('disabled', true).text('loading...');
+      $.ajax({
+        url: form.attr('action'),
+        method: form.attr('method'),
+        data: form.serialize(),
+        success: function (res) {
+          $('body').html(res);
+        }
+      });
+    })
+    .on('click', '#button-next', function () {
+      $.ajax({
+        url: '/play',
+        method: 'POST',
+        data: { button: 'next' },
+        success: function (res) {
+          $('body').html(res);
+        }
+      });
+    })
+    .on('mouseover mouseenter', '.image_container', function() {
+      $(this).droppable ( {
+        accept: "#question",
+        drop: function() {
+          $(this).closest('label').children('input[type=radio]').prop('checked', true).change();
+        }
+      });
+    })
+    .on('mouseover','#question', function() {
+      $(this).draggable({ 
+        revert: "invalid",
+        cursorAt: {left: -5, top: -5} 
+      });
+    })
 });
