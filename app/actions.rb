caef0abbd6a02679417a3a30836ab1d1ede794b4
triple-eh <aleckaleksandrov@gmail.com>
2016-06-18 19:14:36 -0700

    refactored 90%

--- app/actions.rb
+++ app/actions.rb
@@ -3,10 +3,6 @@ helpers do
     session[:game] || session[:game] = Game.new
   end
 
-  def answer_options
-    game.current_question.choices
-  end
-
   def selected_answer
     params[:choice_index].to_i if params[:choice_index]
   end
@@ -17,7 +13,7 @@ helpers do
 
   def answer_class(choice)
     return "" unless choice == selected_answer
-    
+
     if answered_correctly?
       "correct"
     elsif answered_correctly?.nil?
@@ -37,6 +33,7 @@ get '/' do
   erb :index
 end
 
+# new route to properly restart the game
 get '/new_play' do
   session.clear
   redirect '/play'
@@ -48,25 +45,9 @@ end
 
 post '/play' do
 
-  # if params[:choice_index].nil? && params[:button] == ""
-  #   flash[:info] = "You need to make a choice"
-  # end
-
-
-
-
   unless params[:next] == 'true'
-    if answered_correctly?
-      @border_color = 'green'
-      # flash[:info] = "You nailed it!"
-    else
-      @border_color = 'red'
-      # flash[:info] = "Oops... Try again!"
-    end
-
     game.increment_tries
     @selected_answer = selected_answer
-
   else
     game.next_question
     redirect '/end' unless game.current_question
